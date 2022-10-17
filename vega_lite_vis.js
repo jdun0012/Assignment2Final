28a122e1e711d5a4c33417a7e52bac33aaa1e0d0
jdun0012 <64505454+jdun0012@users.noreply.github.com>
2022-10-17 16:14:58 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,36 @@
+var vg_1 = "BirdHabitat.json";
+
+var vg_2 = "BirdHabitatGlobal.json";
+
+var vg_3 = "Frequency.json";
+
+var vg_4 = "Price.json";
+
+var vg_5 = "occurances.json";
+
+vegaEmbed("#line_chart", vg_1).then(function(result) {
+
+    
+ }).catch(console.error);
+
+ vegaEmbed("#chart2", vg_2).then(function(result) {
+
+    
+}).catch(console.error);
+
+vegaEmbed("#chart3", vg_3).then(function(result) {
+
+    
+}).catch(console.error);
+
+vegaEmbed("#chart4", vg_4).then(function(result) {
+
+    
+}).catch(console.error);
+
+vegaEmbed("#chart5", vg_5).then(function(result) {
+
+    
+}).catch(console.error);
+
+
